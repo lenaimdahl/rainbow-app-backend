20acf11d921b312fc11b6bda711830ce3f98ea93
Lena Imdahl <mail@lenaimdahl.de>
2023-07-24 15:58:45 +0200

    first initiation of project, added Activity.model

---
+++ models/Activity.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const activitySchema = new Schema(
+  {
+    type: {
+      type: String,
+      required: [true, "type is required."],
+    },
+    startDate: {
+      type: Date,
+      required: [true, "startdate is required."],
+    },
+    endDate: {
+      type: Date,
+      required: [true, "enddate is required."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Activity = model("activity", activitySchema);
+
+module.exports = Activity;

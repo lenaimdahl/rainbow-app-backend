1be3fe061c5f46104498a840c196b839d0820ebe
Lena Imdahl <mail@lenaimdahl.de>
2023-09-20 17:44:38 +0200

    started saving activities to user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,29 @@
 const router = require("express").Router();
 const UserModel = require("../models/User.model");
 
-router.get("/points", async (req, res) => {
+router.get("/user/userId/activities", async (req, res) => {
   try {
     const user = await UserModel.find();
-    res.status(200).json({ points: user.points });
+    res.status(200).json({ activities: user.activities });
   } catch (err) {
     console.error("ERROR fetching activity from DB", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.post("/user/:userId/activities", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const { userId } = req.params;
+    const updatedActivities = await UserModel.updateOne(
+      { _id: userId },
+      {
+        $push: { activities: id },
+      }
+    );
+    res.status(200).json({ updatedActivities });
+  } catch (err) {
+    console.error("ERROR while adding the activity :>>", err);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });

832a7b9b7b9fc78d69ab6ca8e712a2fa8916eb73
Lena Imdahl <mail@lenaimdahl.de>
2023-09-21 08:30:14 +0200

    fixed route for updating activities for user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,12 +15,14 @@ router.post("/user/:userId/activities", async (req, res) => {
   try {
     const { id } = req.body;
     const { userId } = req.params;
-    const updatedActivities = await UserModel.updateOne(
-      { _id: userId },
+    const updatedActivities = await UserModel.findByIdAndUpdate(
+      userId,
       {
         $push: { activities: id },
-      }
+      },
+      { new: true }
     );
+    console.log(updatedActivities);
     res.status(200).json({ updatedActivities });
   } catch (err) {
     console.error("ERROR while adding the activity :>>", err);
